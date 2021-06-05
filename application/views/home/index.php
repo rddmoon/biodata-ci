4cee78384dc50ee525a16e5ae71eaf5b5be3deae
rddmoon <yaniar0416@gmail.com>
2021-06-05 23:39:44 +0700

    new

---
+++ application/views/home/index.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Yaniar's Biodata</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript">
+addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); }
+</script>
+<link rel="shortcut icon" href="assets/images/icon.png">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link href="assets/css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="assets/css/bootstrap.css" rel='stylesheet' type='text/css'/>
+	    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/move-top.js"></script>
+<script type="text/javascript" src="assets/js/easing.js"></script>
+ <script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+			});
+		});
+	</script>
+</head>
+<body>
+<div class="header" id="head">
+      <div class="container">
+         <div class="header-top">
+      		 <div class="logo">
+				<img src="assets/images/logo.png" alt=""/>
+			 </div>
+		   <div class="top-menu">
+		   	<span class="menu"> </span>
+			<ul>
+			 	<nav class="cl-effect-5">
+				<li><a class="active" href="index.php"><span data-hover="Home">home</span></a></li>
+				<li><a href="#about" class="scroll"><span data-hover="about">about</span></a></li>
+				<li><a href="#contact" class="scroll"><span data-hover="contact">contact</span></a></li>
+				</nav>
+			 </ul>
+			 </div>
+			 <!--script-nav-->
+		 <script>
+		 $("span.menu").click(function(){
+		 $(".top-menu ul").slideToggle("slow" , function(){
+		 });
+		 });
+		 </script>
+			<div class="clearfix"></div>
+		 </div>
+		  <div class="index-banner">
+       	   <div class="wmuSlider example1">
+			   <div class="wmuSliderWrapper">
+				   <article style="position: absolute; width: 100%; opacity: 0;">
+				   	<div class="banner-wrap">
+				   	       	<div class="banner_center">
+				   		 		<h1>Hi,</h1>
+				   		 		<h2>Welcome to <span>my biodata</span></h2>
+				   		 		<h2>Website</h2>
+				   		 		</div>
+				   		 </div>
+					</article>
+				   <article style="position: relative; width: 100%; opacity: 1;">
+				   	   	<div class="banner-wrap">
+				   	      <div class="banner_center">
+				   		 		<h1>Get to know me,</h1>
+				   		 		<h2>Explore the <span>"ABOUT"</span></h2>
+				   		 		<h2>Section now.</h2>
+				   		 		</div>
+				   		  </div>
+				   </article>
+				   <article style="position: absolute; width: 100%; opacity: 0;">
+				   	  	<div class="banner-wrap">
+				   	       <div class="banner_center">
+				   		 		<h1>You can reach me,</h1>
+				   		 		<h2>Check the <span>"CONTACT"</span> section</h2>
+				   		 		<h2>for available contact.</h2>
+				   		 		</div>
+				   		 </div>
+					 </article>
+				 </div>
+
+            </div>
+            <script src="assets/js/jquery.wmuSlider.js"></script>
+			  <script>
+       			$('.example1').wmuSlider();
+   		     </script>
+      </div>
+	 </div>
+	</div>
+	<div class="content">
+		<div class="about-section" id="about" id="about">
+	<div class="container">
+		<div class="about-header">
+			<h3>about</h3>
+			<div class="about-imag">
+			<img src="assets/images/profile.png" alt=""/>
+			</div>
+			<div class="about-text">
+					<p>Hello. My name is Yaniar Pradityas Effendi. I'm a 21-year-old college student. I'm
+            in my third year of studies towards a bachelor's in Informatics at Institut
+            Teknologi Sepuluh Nopember (ITS), Surabaya, Indonesia.
+            <br><br>
+            I was born in Gresik, East Java, Indonesia, and have been living there
+            up until now. My MBTI personality type result is INTJ-A.</p>
+			</div>
+			<div class="clearfix"></div>
+			</div>
+			<div class="about-sectiongrids">
+				<div class="col-md-6 about-leftgrid">
+					<div class="polls">
+					<div class="poll">
+						<div class="poll-desc">
+							<h4>Web Design</h4>
+						</div>
+						<div class="percentage">
+							<p>90%</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="skills">
+			              	<div style="width:90%"> </div>
+	                      </div>
+	                      </div>
+	                      <div class="polls">
+					<div class="poll">
+						<div class="poll-desc">
+							<h4>App Design</h4>
+						</div>
+						<div class="percentage">
+							<p>85%</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="skills">
+			              	<div style="width:85%"> </div>
+	                      </div>
+	                      </div>
+	                      <div class="polls">
+					<div class="poll">
+						<div class="poll-desc">
+							<h4>Ui/Ux</h4>
+						</div>
+						<div class="percentage">
+							<p>85%</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="skills">
+			              	<div style="width:85%"> </div>
+	                      </div>
+	                      </div>
+
+	</div>
+	<div class="col-md-6 about-rightgrid">
+		<div class="polls">
+					<div class="poll">
+						<div class="poll-desc">
+							<h4>Web Development</h4>
+						</div>
+						<div class="percentage">
+							<p>85%</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="skills">
+			              	<div style="width:85%"> </div>
+	                      </div>
+	                      </div>
+        <div class="polls">
+    					<div class="poll">
+    						<div class="poll-desc">
+    							<h4>App Development</h4>
+    						</div>
+    						<div class="percentage">
+    							<p>75%</p>
+    					</div>
+    					<div class="clearfix"></div>
+    				</div>
+    				<div class="skills">
+    			              	<div style="width:75%"> </div>
+    	                      </div>
+    	                      </div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+	<div class="arrow1">
+	<a href="#contact" class="scroll"><img src="assets/images/arrow1.png" alt=""/></a>
+					</div>
+	</div>
+	</div>
+	<!-- about-section-ends -->
+	<div class="footer-section" id="contact" id="contact">
+			<div class="container">
+			<div class="contact-header">
+				<h3> contact</h3>
+        <div class="white">
+          <p>My contact information</p>
+          <!-- <p>Location:</p> -->
+          <div class="icon">
+                <i class="fa fa-map-marker"></i>
+          </div>
+          <p>Kebomas, Gresik, East Java, Indonesia 61122</p>
+          <!-- <p>Phone:</p> -->
+          <div class="icon">
+                <i class="fa fa-phone"></i>
+          </div>
+          <p>+62 812 1757 3735</p>
+          <!-- <p>E-mail:</p> -->
+          <div class="icon">
+                <i class="fa fa-envelope"></i>
+          </div>
+          <p>yaniarpradityas@gmail.com</p>
+        </div>
+			</div>
+			<div class="contact">
+				<form>
+					<div class="col-md-4 contactgrid">
+						<input type="text" class="text" value=" name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = ' name';}">
+					</div>
+					<div class="col-md-4 contactgrid">
+						<input type="text" class="text" value="email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'email';}">
+					</div>
+					<div class="col-md-4 contactgrid">
+						<input type="text" class="text" value="phone" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'phone';}">
+					</div>
+					<div class="col-md-8 contactgrid1">
+						<textarea onfocus="if(this.value == 'Your Message') this.value='';" onblur="if(this.value == '') this.value='Your Message';" >Your Message</textarea>
+					</div>
+					<div class="col-md-4 contactgrid2">
+						<input type="submit" value="[send message]">
+					</div>
+				   <div class="clearfix"></div>
+				</form>
+			</div>
+			<div class="footer-bottom">
+
+					<p>&copy; 2021 yaniarpe . All rights  Reserved</p>
+
+					</div>
+					<script type="text/javascript">
+						$(document).ready(function() {
+							/*
+							var defaults = {
+					  			containerID: 'toTop', // fading element id
+								containerHoverID: 'toTopHover', // fading element hover id
+								scrollSpeed: 1200,
+								easingType: 'linear'
+					 		};
+							*/
+
+							$().UItoTop({ easingType: 'easeOutQuart' });
+
+						});
+					</script>
+				<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+</div>
+
+		</div>
+	</div>
+
+
+</body>
+</html>
